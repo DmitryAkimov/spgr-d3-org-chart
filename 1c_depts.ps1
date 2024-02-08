3dfb2c0bb25ef60eae7175f435c081b2a2369f42
Dmitrii Akimov <52576824+DmitryAkimov@users.noreply.github.com>
2024-02-08 13:41:41 +0300

    доработочки

--- 1c_depts.ps1
+++ 1c_depts.ps1
@@ -8,31 +8,38 @@ $csvPath = 'C:\dev\spgr-d3-org-chart\data';
 
 
 function GetSqlDataSet ([string] $sql , [string] $csvExportPath ) {
-    $sqlConnection = New-Object System.Data.SqlClient.SqlConnection
-    $sqlConnection.ConnectionString = "Server=$server;Database=$database;Integrated Security=True"
-    
-    $sqlCommand = New-Object System.Data.SqlClient.SqlCommand
-    $sqlCommand.CommandText = $sql
-    $sqlCommand.Connection = $sqlConnection
-    
-    $sqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
-    $sqlAdapter.SelectCommand = $sqlCommand
-    
-    $ds = New-Object System.Data.DataSet
-    $sqlAdapter.Fill($ds)
-    $sqlConnection.Close();
-    
-    if($csvExportPath -ne $null) {
-         $ds.Tables[0] | Export-CSV -Path "$csvExportPath" -notypeinformation -Encoding UTF8 -Force
-    }
-    
-    return $ds;
+    try {
+        $sqlConnection = New-Object System.Data.SqlClient.SqlConnection
+        $sqlConnection.ConnectionString = "Server=$server;Database=$database;Integrated Security=True"
+        
+        $sqlCommand = New-Object System.Data.SqlClient.SqlCommand
+        $sqlCommand.CommandText = $sql
+        $sqlCommand.Connection = $sqlConnection
+        
+        $sqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+        $sqlAdapter.SelectCommand = $sqlCommand
+        
+        $ds = New-Object System.Data.DataSet
+        $sqlAdapter.Fill($ds)
+        $sqlConnection.Close();
+        
+        if($csvExportPath -ne $null) {
+            $ds.Tables[0] | Export-CSV -Path "$csvExportPath" -notypeinformation -Encoding UTF8 -Force
+        }
+        
+        return $ds;
+    }        
+    catch { $Error; break;}        
 }
 
     #--------------------------------------------------------------------------
     #  MAIN
     #--------------------------------------------------------------------------
-    cls;
+    CLS;
+
+    #--------------------------------------------------------------------------
+    # Подразделения
+    #--------------------------------------------------------------------------
     $sql = "
     SELECT 
 	    [Код_] as id
@@ -50,47 +57,9 @@ function GetSqlDataSet ([string] $sql , [string] $csvExportPath ) {
     $ds = GetSqlDataSet -sql $sql  -csvExportPath "$csvPath\departments.csv"
     $count = $ds.Tables.Rows.Count;
     Write-Host "Подразделения =  $count";
-    #$ds.Tables[0] | Export-CSV "$csvPath\departments.csv" -notypeinformation -Encoding UTF8 -Force
-
-    #$row_count_sql = $DataSet_dept.Tables.Rows.Count
-    #for ($i = 0; $i -lt $row_count_sql; $i++) {
-
-        # присваиваем данные столбцов переменным
-        #$departmentId = $ds.Tables.Rows[$i].id;
-        #Write-Host "$i = $departmentId";
-        #$departmentId = 'A2CF00505601289111E8FEE8322D2240';
-    #    $sql = "SELECT * FROM (
-    #            SELECT
-	#                'department' as [class]
-	#                ,Код_ as id
-	#                ,IIF ([Уровень]=0, NULL, CONVERT(nvarchar(100),РодительКод,2)) as parentId
-	#                ,Подразделение as [name]
-	#                ,РУК.ФИО as title
-	#                ,NULL as isManager
-    #            FROM 
-	#                [dbo].[fn1СЗУП_СтруктураПредприятия] (0x$departmentId ) СТР
-    #                LEFT JOIN [v1СЗУП_РуководителиПодразделений] РУК ON РУК.СтруктураПредприятияКод=СТР.Код
-	#                UNION ALL
-    #            SELECT
-	#                'employee' as class
-	#                ,КЛС.КодФизЛица as id
-	#                ,CONVERT(nvarchar(100), КЛС.ПодразделениеКод, 2) as parentId
-	#                ,КЛС.ФИО as [name]
-	#                ,КЛС.Должность as title
-	#                ,IIF ( КЛС.КодФизЛица=РУК.КодФизЛица, 1, null ) as isManager
-    #            FROM 
-	#                v1СЗУП_КлассификаторСотрудников_Текущий КЛС
-	#                LEFT JOIN v1СЗУП_РуководителиПодразделений РУК ON РУК.СтруктураПредприятияКод=КЛС.ПодразделениеКод
-    #            WHERE 
-	#                [ПодразделениеКод] IN (SELECT [Код] FROM [dbo].[fn1СЗУП_СтруктураПредприятия] ( 0x$departmentId ) ) 
-    #                AND (РУК.КодФизЛица IS NULL OR КЛС.КодФизЛица<>РУК.КодФизЛица)
-    #            ) as SRC ORDER BY class, name
-    #           ";
-    #    #Write-Host $sql;
-    #    #GetSqlDataSet -sql $sql -csvExportPath "$csvPath\$departmentId.csv"
-    #}
+    
     #--------------------------------------------------------------------------
-    #  все люди
+    #  Сотрудники
     #--------------------------------------------------------------------------
     $sql = "
         SELECT 
@@ -102,9 +71,14 @@ function GetSqlDataSet ([string] $sql , [string] $csvExportPath ) {
             ,[СборЗагрузки] as isTimesheet
             ,[Филиал] as branch
             ,N'https://bitrix.spgr.ru/company/personal/user/' + CONVERT(nvarchar, vBITRIX_users.ID) + '/' as bitrixUserUrl
+			,ДРСИ.РаботаетДистанционно as worksRemotely
+            ,ДРСИ.[СГ_РежимДистанционнойРаботы] as remoteWorkMode
+            ,AdUsers.PhysicalDeliveryOfficeName as physicalDeliveryOfficeName
         FROM
             [v1СЗУП_КлассификаторСотрудников_Текущий] КЛС
             LEFT JOIN vBITRIX_users ON КЛС.КодФизлица=vBITRIX_users.EMPLOYEE_ID
+			LEFT JOIN [v1СЗУП_ДистанционнаяРаботаСотрудниковИнтервальный] ДРСИ ON КЛС.ФизЛицоКод = ДРСИ.ФизЛицоКод AND GETDATE() BETWEEN ДРСИ.[ДатаНачала] AND ДРСИ.[ДатаОкончания]
+            LEFT JOIN AdUsers ON AdUsers.EmployeeId=КЛС.КодФизлица
         ORDER BY
             [Подразделение]
             ,[ФИО]
--- orgchart.js
+++ orgchart.js
@@ -14,6 +14,16 @@ function shortFio (source) {
     return source.replace(/(.+) (.).+ (.).+/, '$1 $2. $3.');
 }
 //=============================================================================
+function testExpand() {
+    let deptId = "A2CF00505601289111E8FEE79511A735";
+    chart.data().forEach (item => {
+        if (item.class=="department" && item.id==deptId) {
+
+        }
+    });
+    chart.setExpanded(deptId).setCentered(deptId).render()
+}
+//=============================================================================
 function isEmpty( val ) {
     // test results
     //---------------
@@ -82,6 +92,7 @@ function filterChart(value) {
 //=============================================================================
 function nodeContent(d, i, arr, state) {
     var nodeClass = "";
+    let upLink =""
     if (d.data.class=="department") {
         hSize = d.depth==0 ? 5 : 7;
         
@@ -94,24 +105,33 @@ function nodeContent(d, i, arr, state) {
         else {
             nodeClass = ""; // обычное подразделение
         }
-
-        return `
-            <div class="card text-center department ${nodeClass}" style="height:${d.height}px;width:${d.width}px;" > 
-    
-                <div class="card-header">
-                    <span>${d.data.department}</span>
+        if (d.data.level!=0 && d.depth==0) {
+            upLink = `
+                <div class="position-absolute top-50 start-100 translate-middle mx-4">
+                    <a href="/?id=${d.data.rawParentId}" class="h1" title="Перейти на уровень выше">
+                        <i class="bi bi-arrow-up-circle-fill text-secondary"></i>
+                    </a>
                 </div>
-                <div class="card-body">
-                    
+            `;
+        }
+        return `
+            <div class="card text-center department ${nodeClass}" style="height:${d.height}px;width:${d.width}px; id="d_${d.data.id}" > 
+                
+                <div class="card-header" style="min-height:3.5em;" >
+                    <span >${d.data.department}</span> ${upLink}
+                </div>
+                <div class="card-body position-relative">
+                    <div class="position-absolute top-50 start-50 translate-middle w-100"> 
                     <h${hSize} class="manager text-truncate"> ${d.depth > 1 ? shortFio(d.data.manager) : d.data.manager}</${hSize}>
                     <!-- <div class=""> ${d.data.id} </div> -->
-                    
+                    </div>
                 </div>
             </div> `
             ;}
 
     else if (d.data.class=="employee") {
-        let branchClass = "bg-secondary";
+        let branchClass = "bg-secondary-subtle";
+        let remotelySvg = ""
         switch (String(d.data.branch).toUpperCase()) {
             case "МСК":
                 branchClass="bg-success-subtle"; break;
@@ -121,15 +141,23 @@ function nodeContent(d, i, arr, state) {
                 branchClass="bg-warning-subtle"; break;
           }
         branchClass += " text-secondary";
+        tooltip = `${d.data.name}\n${d.data.title}\n${d.data.branch}\n${d.data.physicalDeliveryOfficeName}\n${d.data.id}\n`;
+        if ("worksRemotely" in d.data && String(d.data.worksRemotely).toUpperCase()=="TRUE") {
+            tooltip += `\nДистанционно ${String(d.data.remoteWorkMode)}`;
+            remotelySvg =`
+            <div  class="homeuser position-absolute top-0 start-100 translate-middle" style="width:2em;">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path opacity="0.5" d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c.2 35.5-28.5 64.3-64 64.3H128.1c-35.3 0-64-28.7-64-64V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24zM352 224a64 64 0 1 0 -128 0 64 64 0 1 0 128 0zm-96 96c-44.2 0-80 35.8-80 80c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16c0-44.2-35.8-80-80-80H256z"/></svg>
+            </div>`;
+        }
         return `
-            <div class="card text-center employee rounded-4 position-relative" style="height:${d.height}px;width:${d.width}px;" data-bs-toggle="tooltip" data-bs-title="Tooltip on top">
+            <div class="card text-center employee rounded-4 position-relative ${nodeClass}" style="height:${d.height}px;width:${d.width}px;" data-bs-toggle="tooltip" title="${tooltip}" data-bs-placement="top">
                 <div class="position-absolute top-50 start-50 translate-middle w-100"> 
                     <div class="name text-truncate mx-2">  ${d.data.name} </div>
                     <div class="title fst-italic text-body-secondary text-truncate mt-1 mx-2">  ${d.data.title} </div>
                 </div>
 
                 <div class="badge position-absolute ${branchClass} top-100 start-100 translate-middle"  style="width:3.5em;">${d.data.branch}</div>
-                
+                ${remotelySvg}
              </div>
         `
         };
@@ -179,7 +207,8 @@ function filterStaff (data) {
 function onNodeClick (d){
     if (d.data.class=="department") {
         if (d.depth > 0) {
-            window.open(`index.html?id=${d.data.id}`, '_blank') ;
+            //window.open(`/?id=${d.data.id}`) ;
+            document.location.href = `/?id=${d.data.id}`;
         }
     } else if (d.data.class=="employee" && "bitrixUserUrl" in d.data) {
         window.open(`${d.data.bitrixUserUrl}`, '_blank') ;
@@ -236,7 +265,7 @@ d3
         staff = data;
         d3.csv('./data/departments.csv')
         .then((data) => {
-            data.forEach( item => item.class = 'department');
+            data.forEach( item => { item.class = 'department'; item.rawParentId=item.parentId });
             departments = data;
             drawOrgchart(rootDepartmentId);
         })
